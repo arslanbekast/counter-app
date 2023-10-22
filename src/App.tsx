0fbcb8fb5b8022fb8a205665b79ea6895ddc8adb
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-22 20:14:22 +0300

    Refactoring App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Counter} from "./components/Counter/Counter";
+import {Settings} from "./components/Settings/Settings";
 
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Settings/>
+            <Counter/>
+        </div>
+    );
 }
 
 export default App;
