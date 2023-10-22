764343570d02f78ed53439328efd3f5e04112ead
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-22 20:40:25 +0300

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Counter} from "./components/Counter/Counter";
 import {Settings} from "./components/Settings/Settings";
 
 function App() {
+
+    const minCount: number = 0;
+    const maxCount: number = 5;
+
+    const [count, setCount] = useState<number>(0)
+
+    const incCount = () => count < maxCount && setCount(prevCount => prevCount + 1)
+    const resetCount = () => setCount(minCount)
+
     return (
         <div className="App">
             <Settings/>
-            <Counter/>
+            <Counter count={count}
+                     minCount={minCount}
+                     maxCount={maxCount}
+                     incCount={incCount}
+                     resetCount={resetCount}/>
         </div>
     );
 }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, {FC, useState} from 'react';
 import './Counter.css'
 import {Scoreboard} from "../Scoreboard/Scoreboard";
 import {Buttons} from "../Buttons/Buttons";
 
-export const Counter = () => {
+type CounterPropsType = {
+    count: number
+    minCount: number
+    maxCount: number
+    incCount: () => void
+    resetCount: () => void
+}
 
-    const minCount: number = 0;
-    const maxCount: number = 5;
-
-    const [count, setCount] = useState<number>(0)
-
-    const incCount = () => count < maxCount && setCount(prevCount => prevCount + 1)
-    const resetCount = () => setCount(minCount)
+export const Counter: FC<CounterPropsType> = ({count, minCount, maxCount, incCount, resetCount}) => {
 
     return (
         <div className='counter'>
