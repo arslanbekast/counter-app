b03d6a122e25b5da4def9b0c75ae9dcbafc1d652
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-05 12:41:11 +0300

    Changed Counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,18 +7,21 @@ type CounterPropsType = {
     count: number
     minCount: number
     maxCount: number
+    editing: boolean
+    error: boolean
     incCount: () => void
     resetCount: () => void
 }
 
-export const Counter: FC<CounterPropsType> = ({count, minCount, maxCount, incCount, resetCount}) => {
+export const Counter: FC<CounterPropsType> = ({count, minCount, maxCount, editing, error, incCount, resetCount}) => {
 
     return (
         <div className='counter'>
-            <Scoreboard count={count} maxCount={maxCount}/>
+            <Scoreboard count={count} maxCount={maxCount} editing={editing} error={error}/>
             <Buttons count={count}
                      minCount={minCount}
                      maxCount={maxCount}
+                     editing={editing}
                      incCount={incCount}
                      resetCount={resetCount}/>
         </div>
