9c6104b6069c62375540aea6c3592eb6fc29dd1f
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-05 12:41:26 +0300

    Changed Buttons component

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -6,16 +6,17 @@ type ButtonsPropsType = {
     count: number
     minCount: number
     maxCount: number
+    editing: boolean
     incCount: () => void
     resetCount: () => void
 }
 
-export const Buttons: FC<ButtonsPropsType> = ({count, minCount, maxCount, incCount, resetCount}) => {
+export const Buttons: FC<ButtonsPropsType> = ({count, minCount, maxCount, editing, incCount, resetCount}) => {
 
     return (
         <div className="btn-group">
-            <Button title='Inc' disabled={count===maxCount} callback={incCount}/>
-            <Button title='Reset' disabled={count===minCount} callback={resetCount}/>
+            <Button title='Inc' disabled={editing || count===maxCount} callback={incCount}/>
+            <Button title='Reset' disabled={editing || count===minCount} callback={resetCount}/>
         </div>
     );
 };
