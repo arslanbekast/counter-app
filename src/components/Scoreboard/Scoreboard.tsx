68b034627c3a91c2495f7e5238a1335d6c96a1b4
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-05 12:41:40 +0300

    Changed Scoreboard component

--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -4,12 +4,22 @@ import './Soreboard.css'
 type ScoreboardPropsType = {
     count: number
     maxCount: number
+    editing: boolean
+    error: boolean
 }
 
-export const Scoreboard: FC<ScoreboardPropsType> = ({count, maxCount}) => {
+export const Scoreboard: FC<ScoreboardPropsType> = ({count, maxCount, editing, error}) => {
+
+    const editingMessage = <span style={{fontSize: "14px"}}>Enter values and press 'Set'</span>;
+    const editingErrorMessage = <span style={{fontSize: "14px", color: "red"}}>Incorrect value</span>;
+
     return (
         <span className={count < maxCount ? 'scoreboard' : 'scoreboard limitCount'}>
-            {count}
+            {error
+                ? editingErrorMessage
+                : editing
+                ? editingMessage
+                : count}
         </span>
     );
 };
