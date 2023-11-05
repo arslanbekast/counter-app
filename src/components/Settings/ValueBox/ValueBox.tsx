f999acc3a8a97335995353a4723032b7a2f37eb7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-05 12:42:11 +0300

    Changed ValueBox component

--- src/components/Settings/ValueBox/ValueBox.tsx
+++ src/components/Settings/ValueBox/ValueBox.tsx
@@ -4,13 +4,18 @@ import {Input} from "../../Input/Input";
 
 type ValueBoxPropsType = {
     title: string
+    defaultValue: number
+    error: boolean
+    setEditing: (editing: boolean) => void
+    callback: (number: number) => void
 }
 
-export const ValueBox: FC<ValueBoxPropsType> = ({title}) => {
+export const ValueBox: FC<ValueBoxPropsType> = ({title, defaultValue, error, setEditing, callback}) => {
+    console.log('Default value: '+defaultValue)
     return (
         <div className="valueBox">
             <span>{title}</span>
-            <Input />
+            <Input defaultValue={defaultValue} error={error} setEditing={setEditing} callback={callback}/>
         </div>
     );
 };
