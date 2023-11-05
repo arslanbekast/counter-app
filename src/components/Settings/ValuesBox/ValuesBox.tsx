3cdb29281e2269aab5040a5e9c9e2bad2cf4682d
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-05 12:42:04 +0300

    Changed ValuesBox component

--- src/components/Settings/ValuesBox/ValuesBox.tsx
+++ src/components/Settings/ValuesBox/ValuesBox.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {ValueBox} from "../ValueBox/ValueBox";
 import './ValuesBox.css'
 
-export const ValuesBox = () => {
+type ValuesBoxPropsType = {
+    minCount: number
+    maxCount: number
+    setMinCount: (minCount: number) => void
+    setMaxCount: (maxCount: number) => void
+    setEditing: (editing: boolean) => void
+}
+
+export const ValuesBox: FC<ValuesBoxPropsType> = ({minCount, maxCount, setMinCount, setMaxCount, setEditing}) => {
+    console.log('Min count: '+minCount)
+    console.log('Max count: '+maxCount)
     return (
         <div className='valuesBox'>
-            <ValueBox title="Max value" />
-            <ValueBox title="Min value" />
+            <ValueBox title="Max value:"
+                      defaultValue={maxCount}
+                      error={maxCount < 0 || minCount >= maxCount}
+                      setEditing={setEditing}
+                      callback={setMaxCount}/>
+
+            <ValueBox title="Start value:"
+                      defaultValue={minCount}
+                      error={minCount < 0 || minCount >= maxCount}
+                      setEditing={setEditing}
+                      callback={setMinCount}/>
         </div>
     );
 };
